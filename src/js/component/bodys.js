1ca19753ce6895526ac7f6c70287c17d61d24ce6
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2024-08-20 05:21:35 +0000

    codificando la landing page

---
+++ src/js/component/bodys.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function Bodys(){
+    return(
+        <div className="conteiner ">
+          <div className="row-md-12 justify-content-center bg-light">
+            <div className="colmd-12">
+              <div className="row mt-3 justify-content-center">
+                <div className="col-md-10">
+                  <div className="jumbotron">
+                    <h1 className="display-4">Hello, world!</h1>
+                    <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>                           
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                  </div>  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+              
+    ); 
+};
+
+export default Bodys;
---
+++ src/js/component/cards.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Cards(){
+    return(
+        <div className="row-md-4">
+            <div className="col justufy-content-evenly">
+                <div className="card" style="width: 18rem;">
+                    <img className="card-img-top w-500 h-325" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9qgt6J7GoHd9UfGRNqrTIsgW4TRMv-YdJIg&s"  alt="..."/>
+                        <div className="card-body text-center">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+            
